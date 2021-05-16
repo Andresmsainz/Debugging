606f807e400419180924c1da95bd4013f3541a49
Andres Sainz <andresmsainz@gmail.com>
2021-05-16 13:59:57 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Debugging
+{
+    class Program
+    {
+        static int x = 0;
+        static int y = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Debugging!");
+
+            x = 2;
+            y = 2;
+            Console.WriteLine("Inside Main");
+
+            ChangeValues();
+
+            x = 10;
+            y = 20;
+            Console.WriteLine("Inside Main Again");
+        }
+
+        public static void ChangeValues()
+        {
+            x = 4;
+            y = 5;
+            Console.WriteLine("Inside Change Values");
+        }
+    }
+}
